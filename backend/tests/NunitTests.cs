1e42a968379fc33ca7480052083ef8f1775a8034
Stewart Henderson <stewart.henderson@protonmail.com>
2019-01-22 20:40:18 -0600

    Removed reflection

--- backend/tests/NunitTests.cs
+++ backend/tests/NunitTests.cs
@@ -9,7 +9,6 @@ public class OnlyTest
     [Test]
     public void IntegerEquals() 
     {
-        System.Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod.ToString());
         int a = 10;
         Assert.AreEqual(10, a);
     }
@@ -17,7 +16,6 @@ public class OnlyTest
     [Test]
     public void StringEquals() 
     {
-        System.Console.WriteLine(System.Reflection.MethodBase.GetCurrentMethod.ToString());
         string a = "a";
         Assert.AreEqual("a", a);
     }
